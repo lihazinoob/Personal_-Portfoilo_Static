8e6c87c727d9b6ba749866d5935159f9b463e5ae
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-08-19 01:39:52 +0600

    About section is designed.

--- port.js
+++ port.js
@@ -1,21 +1,16 @@
-const hamburger = document.querySelector('.menuham');
-const navLinks = document.querySelector('.list');
-const linksfornavigation = document.querySelectorAll('.navlink');
+const hamburger = document.querySelector(".menuham");
+const navLinks = document.querySelector(".list");
+const linksfornavigation = document.querySelectorAll(".navlink");
 
-hamburger.addEventListener('click', function()
-{
-  hamburger.classList.toggle('is-active');
-  navLinks.classList.toggle('open');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("is-active");
+  navLinks.classList.toggle("open");
   console.log("clicked");
-
 });
 
-linksfornavigation.forEach(link => {
-  link.addEventListener('click', function()
-  {
-    hamburger.classList.remove('is-active');
-    navLinks.classList.remove('open');
+linksfornavigation.forEach((link) => {
+  link.addEventListener("click", function () {
+    hamburger.classList.remove("is-active");
+    navLinks.classList.remove("open");
   });
-  });
-
-
+});
